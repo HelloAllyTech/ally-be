2b72b451bea0cdb2146a92d665b4600368131a06
antonyvibin-kv <antony.v@keyvalue.systems>
2025-02-21 20:46:30 +0530

    code refactor

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -25,7 +25,6 @@ import { Message, MessageType } from '../common/entities/message.entity';
 
 @WebSocketGateway({
   cors: { origin: '*' },
-  namespace: 'ws', // Ensure client connects to this namespace
 })
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   private sessions: { [key: string]: UserChatSessionData } = {};
