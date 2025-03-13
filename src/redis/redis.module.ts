93829a7709da1e4805f109db54844c9fa98a21c7
antonyvibin-kv <antony.v@keyvalue.systems>
2025-03-13 14:53:26 +0530

    redis port issue fix

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -9,7 +9,8 @@ import { RedisModule as NestRedisModule } from '@liaoliaots/nestjs-redis';
       config: [
         {
           name: 'default',
-          url: process.env.REDIS_URL,
+          host: process.env.REDIS_HOST,
+          port: parseInt(process.env.REDIS_PORT || '6379'),
         },
       ],
     }),
