6dba2419ecbaf9a706923ebe14b1c028cbe0a825
antonyvibin-kv <antony.v@keyvalue.systems>
2025-04-25 13:12:40 +0530

    ignored 401 on slack alerts

--- src/notification/service/notification.service.ts
+++ src/notification/service/notification.service.ts
@@ -4,9 +4,13 @@ import { SlackService } from './slack.service';
 
 @Injectable()
 export class NotificationService {
+  private ignoreStatusCode = [401];
   constructor(private readonly slackService: SlackService) {}
   handleException(payload: NotificationErrorType) {
     const { statusCode, timestamp, path, message, type } = payload;
+    if (this.ignoreStatusCode.includes(statusCode)) {
+      return;
+    }
     const slackMessage = `*${type}* - ${message} - ${statusCode} - ${path} - ${timestamp}`;
     this.slackService.sendMessage(slackMessage);
   }
