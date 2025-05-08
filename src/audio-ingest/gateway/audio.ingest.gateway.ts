19b069fd0c5552ca8796eba0b1271393c5e52195
antonyvibin-kv <antony.v@keyvalue.systems>
2025-05-08 10:46:11 +0530

    gateway fix

---
+++ src/audio-ingest/gateway/audio.ingest.gateway.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import * as WebSocket from 'ws';
+import { Server } from 'http';
+import { LoggerService } from '../../logger/logger.service';
+
+@Injectable()
+export class AudioIngestGateway {
+  private wss: WebSocket.Server | undefined;
+  private logger = LoggerService.getInstance(AudioIngestGateway.name);
+
+  initialize(httpServer: Server) {
+    this.logger.info('Initializing Audio Ingest Gateway');
+    this.wss = new WebSocket.Server({ server: httpServer, path: '/ws' });
+
+    this.wss.on('connection', (ws) => {
+      let isAlive = true;
+      console.log('3rd-party WS client connected');
+      ws.send('Hello from the server!!!!!sldkvcldkfv');
+
+      ws.on('pong', () => {
+        isAlive = true;
+      });
+
+      ws.on('message', (msg) => {
+        console.log('Received from 3rd party:', msg.toString());
+      });
+
+      // send ping every 30 seconds
+      const pingInterval = setInterval(() => {
+        if (isAlive) {
+          this.logger.info('Sending ping to 3rd-party WS client');
+          ws.ping();
+        }
+      }, 1000);
+
+      ws.on('close', () => {
+        clearInterval(pingInterval);
+        this.logger.info('3rd-party WS client disconnected');
+      });
+    });
+  }
+}
