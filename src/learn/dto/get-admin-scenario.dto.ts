2cae2dc09167356b97e4daf8c0d79b7f336e005b
Afna <afna@keyvalue.systems>
2025-11-17 11:28:32 +0530

    feat: add getAdminScenario endpoint and update GetAdminScenarioDto for improved scenario retrieval

--- src/learn/dto/get-admin-scenario.dto.ts
+++ src/learn/dto/get-admin-scenario.dto.ts
@@ -16,14 +16,5 @@ class TerminationEventDto {
 }
 
 export class GetAdminScenarioDto extends Scenarios {
-  @ApiProperty({
-    required: false,
-    example: {
-      eventId: '123e4567-e89b-12d3-a456-426614174000',
-      name: 'Termination Event',
-      autoTerminationStatus: true,
-      message: 'Termination message',
-    },
-  })
   terminationEvent?: TerminationEventDto;
 }
