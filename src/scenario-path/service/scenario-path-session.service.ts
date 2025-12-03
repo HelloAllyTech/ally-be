27d432b210eb2c01d67af09b8d10daefa423706c
Afna <afna@keyvalue.systems>
2025-12-03 11:15:36 +0530

    revert: add eventStatus column to scenario_sessions and update end of session

--- src/scenario-path/service/scenario-path-session.service.ts
+++ src/scenario-path/service/scenario-path-session.service.ts
@@ -253,7 +253,6 @@ export class ScenarioPathSessionService {
       transitionMessageTitle: currentPathItem?.messageTitle,
       transitionMessageContent: currentPathItem?.messageContent,
       isScenarioPathSessionCompleted: !!currentScenarioPathSession?.completedAt,
-      eventStatus: currentScenarioSession?.eventStatus,
     };
     if (
       currentPathSessionItem?.status !== SessionItemStatus.COMPLETED ||
@@ -301,7 +300,7 @@ export class ScenarioPathSessionService {
     callDuration = 0,
   }: {
     scenarioPathSessionItemId: string;
-    score?: number;
+    score: number;
     callDuration: number;
   }) {
     const userId = ExecutionManager.getUserId();
@@ -337,7 +336,7 @@ export class ScenarioPathSessionService {
     // Score less than minimum score -> cant make the session complete
     if (
       currentScenarioPathItem?.minimumScore !== undefined &&
-      (score ?? 0) < currentScenarioPathItem?.minimumScore
+      score < currentScenarioPathItem?.minimumScore
     )
       return;
 
