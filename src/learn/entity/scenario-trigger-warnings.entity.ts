339c91fab35381f5557c83a9f76ab84e7a8f4c59
Afna <afna@keyvalue.systems>
2025-12-04 10:39:28 +0530

    feat: add trigger warnings functionality with database migration, create and get APIs

---
+++ src/learn/entity/scenario-trigger-warnings.entity.ts
@@ -0,0 +1,17 @@
+import { BaseWithoutTenantEntity } from 'src/common/entity/base-without-tenant.entity';
+import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('scenario_trigger_warnings')
+@Index(['scenarioId', 'triggerWarningId'], {
+  unique: true,
+})
+export class ScenarioTriggerWarnings extends BaseWithoutTenantEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column()
+  scenarioId!: number;
+
+  @Column()
+  triggerWarningId!: string;
+}
---
+++ src/learn/entity/trigger-warnings.entity.ts
@@ -0,0 +1,12 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+import { BaseWithoutTenantEntity } from 'src/common/entity/base-without-tenant.entity';
+
+@Entity('trigger_warnings')
+export class TriggerWarnings extends BaseWithoutTenantEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column()
+  name!: string;
+}
