1674be22e171b3f3855adebfa2c421407a791809
shubhambhoite502 <60102581+shubhambhoite502@users.noreply.github.com>
2025-12-10 11:42:35 +0530

    fix: create languages table maigration fixed

--- src/database/migrations/1765343876000-createLanguagesTable1765343876000.ts
+++ src/database/migrations/1765343876000-createLanguagesTable1765343876000.ts
@@ -23,18 +23,18 @@ export class CreateLanguagesTable1765343876000 implements MigrationInterface {
 
     // Insert languages data
     await queryRunner.query(`
-      INSERT INTO "languages" ("id", "value", "label", "active", "translationCode") VALUES
-        (1, 'en-IN', 'English (India)', true, 'en'),
-        (DEFAULT, 'hi-IN', 'Hindi (India)', true, 'hi'),
-        (DEFAULT, 'bn-IN', 'Bengali (India)', true, 'bn'),
-        (DEFAULT, 'te-IN', 'Telugu (India)', true, 'te'),
-        (DEFAULT, 'mr-IN', 'Marathi (India)', true, 'mr'),
-        (DEFAULT, 'ta-IN', 'Tamil (India)', true, 'ta'),
-        (DEFAULT, 'gu-IN', 'Gujarati (India)', true, 'gu'),
-        (DEFAULT, 'kn-IN', 'Kannada (India)', true, 'kn'),
-        (DEFAULT, 'ml-IN', 'Malayalam (India)', true, 'ml'),
-        (DEFAULT, 'pa-IN', 'Punjabi (India)', true, 'pa'),
-        (DEFAULT, 'or-IN', 'Odia (India)', true, 'or')
+        INSERT INTO "languages" ("id", "value", "label", "active", "translationCode") VALUES
+          (DEFAULT, 'en-IN', 'English (India)', true, 'en'),
+          (DEFAULT, 'hi-IN', 'Hindi (India)', true, 'hi'),
+          (DEFAULT, 'bn-IN', 'Bengali (India)', true, 'bn'),
+          (DEFAULT, 'te-IN', 'Telugu (India)', true, 'te'),
+          (DEFAULT, 'mr-IN', 'Marathi (India)', true, 'mr'),
+          (DEFAULT, 'ta-IN', 'Tamil (India)', true, 'ta'),
+          (DEFAULT, 'gu-IN', 'Gujarati (India)', true, 'gu'),
+          (DEFAULT, 'kn-IN', 'Kannada (India)', true, 'kn'),
+          (DEFAULT, 'ml-IN', 'Malayalam (India)', true, 'ml'),
+          (DEFAULT, 'pa-IN', 'Punjabi (India)', true, 'pa'),
+          (DEFAULT, 'or-IN', 'Odia (India)', true, 'or')
     `);
   }
 
