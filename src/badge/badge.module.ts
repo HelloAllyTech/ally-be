60144ce2f55298bd6de09c94286bfab3ac660b15
Afna <afna@keyvalue.systems>
2026-01-14 16:21:46 +0530

    feat: add BadgeGroup entity and repository, update Badge module and constants

--- src/badge/badge.module.ts
+++ src/badge/badge.module.ts
@@ -3,13 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BadgeController } from './controller/badge.controller';
 import { BadgeService } from './service/badge.service';
 import { BadgeRepository } from './repository/badge.repository';
+import { BadgeGroupRepository } from './repository/badge-group.repository';
 import { Badge } from './entity/badge.entity';
 import { BadgeUser } from './entity/badge-user.entity';
+import { BadgeGroup } from './entity/badge-group.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Badge, BadgeUser])],
+  imports: [TypeOrmModule.forFeature([Badge, BadgeUser, BadgeGroup])],
   controllers: [BadgeController],
-  providers: [BadgeService, BadgeRepository],
-  exports: [BadgeService, BadgeRepository],
+  providers: [BadgeService, BadgeRepository, BadgeGroupRepository],
+  exports: [BadgeService, BadgeRepository, BadgeGroupRepository],
 })
 export class BadgeModule {}
