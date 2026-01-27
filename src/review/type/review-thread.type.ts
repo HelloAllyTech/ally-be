d1468b83aff60558656a7e2d78fbfbee33f62b11
Sachin Jose <sachin@keyvalue.systems>
2026-01-27 18:25:39 +0530

    feat: add comments count retrieval by review IDs with hidden comments filter

--- src/review/type/review-thread.type.ts
+++ src/review/type/review-thread.type.ts
@@ -3,3 +3,8 @@ import { Pagination } from 'src/common/type/common.type';
 export interface GetReviewThreadsOptions extends Pagination {
   includeMessage?: boolean;
 }
+
+export interface ReviewCommentCount {
+  reviewId: string;
+  count: number;
+}
