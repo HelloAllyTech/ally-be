e8a1878b4f38bcda6167789bff79d1bdb9aeded8
shubhambhoite502 <shubhambhoite502@gmail.com>
2026-02-13 10:11:56 +0530

    feat: migrate OpenAI prompt logic to database-backed system

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -2,9 +2,11 @@ import { Global, Module } from '@nestjs/common';
 import { CryptoService } from './service/crypto.service';
 import { GoogleTranslationsService } from './service/google-translation.service';
 import { OpenAITranslationsService } from './service/openai-translation.service';
+import { PromptModule } from 'src/prompt/prompt.module';
 
 @Global()
 @Module({
+  imports: [PromptModule],
   providers: [
     CryptoService,
     GoogleTranslationsService,
