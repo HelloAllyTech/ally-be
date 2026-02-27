3a0b420532d97ab7c6091037ee552ef4bd4a4dc1
Afna <afna@keyvalue.systems>
2026-02-27 17:21:42 +0530

    feat: enhance scenario cover image library with support for multiple image uploads

--- src/scenario-cover-image-library/dto/add-scenario-cover-image.dto.ts
+++ src/scenario-cover-image-library/dto/add-scenario-cover-image.dto.ts
@@ -1,18 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString, Matches } from 'class-validator';
-
-const S3_URL_PATTERN = /^https:\/\/[^.]+\.s3\.[^.]+\.amazonaws\.com\/(.+)$/;
+import { IsNotEmpty, IsString, IsUrl } from 'class-validator';
 
 export class AddScenarioCoverImageDto {
   @ApiProperty({
-    description: 'S3 URL of the uploaded image',
+    description:
+      'Image URL - S3 URL (from presigned upload) or any public image asset URL',
     example:
       'https://my-bucket.s3.us-east-1.amazonaws.com/scenario-cover-image-library/1730000000000-cover.jpg',
   })
   @IsString()
   @IsNotEmpty()
-  @Matches(S3_URL_PATTERN, {
-    message: 'imageUrl must be a valid S3 URL',
-  })
+  @IsUrl({ protocols: ['http', 'https'] })
   imageUrl!: string;
 }
