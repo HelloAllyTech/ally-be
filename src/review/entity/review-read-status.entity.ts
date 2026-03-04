ab2f65a89c739e35d4ea6e55c7427ddbec22a5d3
Mayur Maskar <141343927+maxyur077@users.noreply.github.com>
2026-03-04 15:40:24 +0530

    feat:fixed code review changes

--- src/review/entity/review-read-status.entity.ts
+++ src/review/entity/review-read-status.entity.ts
@@ -1,7 +1,7 @@
 import { Column, CreateDateColumn, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('review_read_status')
-@Index('IDX_review_read_status_user_review', ['userId', 'reviewId'], { unique: true })
+@Index('uq_review_read_status_user_id_review_id_idx', ['userId', 'reviewId'], { unique: true })
 export class ReviewReadStatus {
   @PrimaryGeneratedColumn('uuid')
   id!: string;
