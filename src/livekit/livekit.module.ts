d516dfdf4764cac95f2665268cc9d0b3ea05b1b9
Sachin Jose <sachin@keyvalue.systems>
2026-04-07 19:42:12 +0530

    Revert "feat: Implement egress started event handling for getting the correct start time"
    This reverts commit 628f15e054305b519b84fced241dca1240107c05.

--- src/livekit/livekit.module.ts
+++ src/livekit/livekit.module.ts
@@ -5,18 +5,12 @@ import { LiveKitController } from './controller/livekit.controller';
 import { LivekitWebhookController } from './webhook/livekit-webhook.controller';
 import { ParticipantJoinedHandler } from './webhook/handlers/participant-joined.handler';
 import { RoomFinishedHandler } from './webhook/handlers/room-finished.handler';
-import { EgressStartedHandler } from './webhook/handlers/egress-started.handler';
 import { LearnModule } from 'src/learn/learn.module';
 
 @Module({
   imports: [ConfigModule, forwardRef(() => LearnModule)],
   controllers: [LiveKitController, LivekitWebhookController],
-  providers: [
-    LiveKitService,
-    ParticipantJoinedHandler,
-    RoomFinishedHandler,
-    EgressStartedHandler,
-  ],
+  providers: [LiveKitService, ParticipantJoinedHandler, RoomFinishedHandler],
   exports: [LiveKitService],
 })
 export class LiveKitModule {}
