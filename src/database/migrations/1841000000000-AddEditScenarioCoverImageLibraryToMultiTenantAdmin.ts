5203d88c1d62fcc236e8068aaef859fd0179a259
shubhambhoite502 <shubhambhoite502@gmail.com>
2026-07-14 23:45:09 +0530

    Add edit scenario cover image library permission to multi-tenant admin

---
+++ src/database/migrations/1841000000000-AddEditScenarioCoverImageLibraryToMultiTenantAdmin.ts
@@ -0,0 +1,30 @@
+import { UserRole } from 'src/common/constants/user.constants';
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddEditScenarioCoverImageLibraryToMultiTenantAdmin1841000000000 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+           INSERT INTO "group_permissions" ("groupId", "permissionId")
+           SELECT g."id", p."id"
+           FROM "groups" g
+           JOIN "permissions" p ON p."name" IN ('edit:scenario-cover-image-library')
+           WHERE g."name" = '${UserRole.MULTI_TENANT_ADMIN}'
+         `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+         DELETE FROM "group_permissions"
+         WHERE "groupId" IN (
+           SELECT "id"
+           FROM "groups"
+           WHERE "name" = '${UserRole.MULTI_TENANT_ADMIN}'
+         )
+         AND "permissionId" IN (
+           SELECT "id"
+           FROM "permissions"
+           WHERE "name" IN ('edit:scenario-cover-image-library')
+         )
+       `);
+  }
+}
